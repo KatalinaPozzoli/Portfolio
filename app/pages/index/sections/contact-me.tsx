f7a66044426cf330eee503d583265b3bd45b86f1
KatalinaPozzoli <65791981+KatalinaPozzoli@users.noreply.github.com>
2022-03-08 17:09:51 -0300

    feat: implement captcha verification

--- app/pages/index/sections/contact-me.tsx
+++ app/pages/index/sections/contact-me.tsx
@@ -12,9 +12,26 @@ import {
     PortfolioButton
 } from "~/pages/index/index.styled";
 import ContactFormMeImage from '../../../../public/assets/KPContactMeImage.png'
-import {forwardRef} from "react";
+import {forwardRef, useEffect, useRef, useState} from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import useContactForm from "~/hooks/useContactForm.hook";
+
+interface ContactForm {
+    name: string
+    email: string
+    message: string
+    captcha: string
+}
 
 const ContactMe = forwardRef<HTMLElement>(({}, ref) => {
+    const captcha = useRef<ReCAPTCHA>()
+    const {formIsValid, setFormValue} = useContactForm({
+        name: '',
+        email: '',
+        message: '',
+        captcha: ''
+    })
+
     return (
         <ContactMeContainer ref={ref}>
             <InnerContainer>
@@ -23,10 +40,22 @@ const ContactMe = forwardRef<HTMLElement>(({}, ref) => {
                     <FormBodySectionContainer>
                         <ContactMeTitle>Lets Talk!</ContactMeTitle>
                         <ContactMeDescription>I’d love to know about you</ContactMeDescription>
-                        <ContactMeForm method="post" action="?index">
-                            <ContactMeInput type="text" placeholder="Name" name="name"/>
-                            <ContactMeInput type="email" placeholder="Email address" name="email"/>
-                            <ContactMeTextarea placeholder="Anything you'd like to tell me..." name="message"></ContactMeTextarea>
+                        <ContactMeForm onSubmit={event => !formIsValid ? event.preventDefault() : true} method="post" action="?index">
+                            <ContactMeInput
+                                onChange={({target}) => setFormValue('name', target.value)}
+                                type="text" placeholder="Name" name="name" required/>
+                            <ContactMeInput
+                                onChange={({target}) => setFormValue('email', target.value)}
+                                type="email" placeholder="Email address" name="email" required/>
+                            <ContactMeTextarea
+                                onChange={({target}) => setFormValue('message', target.value)}
+                                required placeholder="Anything you'd like to tell me..."
+                                               name="message"></ContactMeTextarea>
+                            <ReCAPTCHA
+                                ref={captcha as any}
+                                sitekey="6LfW4cMeAAAAAGDRPxz2TnR7icFynSSdPC8n8M9F"
+                                onChange={(value) => value && setFormValue('captcha', value)}
+                            />
                             <PortfolioButton type="submit"> Submit </PortfolioButton>
                         </ContactMeForm>
                     </FormBodySectionContainer>
